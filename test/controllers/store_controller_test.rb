ea8c4c56cb6ed58c915da3dcdc6c3784b7363a1a
Alexey Vasilyev <bestspajic@gmail.com>
2015-11-15 10:05:51 -0800

    check that StoreController assignes @products and resolve #17

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -5,13 +5,23 @@ class StoreControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+  
+  test "should assign instance variable '@products'" do
+    assert_not_nil assigns(:products)
+  end
 
   test "should get properly rendered store/index view" do
     get :index
     assert_response :success
     assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.entry', minimum: 3
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+  test "should get properly rendered store/index view inside app layout" do
+    get :index
+    assert_response :success
     assert_select '#main .entry', minimum: 3
     assert_select '#columns #side a', minimum: 4
-    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
