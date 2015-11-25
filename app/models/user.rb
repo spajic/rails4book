2e198c6f76c7a0ad27bdaee7b77bf744003a0860
Alexey Vasilyev <bestspajic@gmail.com>
2015-11-25 14:51:07 -0800

    dont let to delete last admin to resolve #50

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  after_destroy :ensure_at_least_one_admin_remains
   has_secure_password
+
+  private
+  	def ensure_at_least_one_admin_remains
+  		if User.count.zero?
+  			raise "Последний администратор не может быть удалён"
+  		end
+  	end
 end
