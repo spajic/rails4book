b042558a8df90f912cb79ff7e18c13d41dde2f38
Alexey Vasilyev <bestspajic@gmail.com>
2015-11-25 12:27:49 -0800

    add User model with password:digest to resolve #46

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :orders
   resources :cart_items
   resources :carts
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118191933) do
+ActiveRecord::Schema.define(version: 20151125200535) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20151118191933) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
